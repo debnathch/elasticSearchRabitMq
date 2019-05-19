0d0c7e7ff3c85cc43a2b013c170ec5d956af0bad
debnathchatterjee <debnathch@gmail.com>
2019-05-20 03:20:17 +0530

    FIRST COMMIT

--- src/main/java/com/example/blog/dao/repository/CommentSearchRepository.java
+++ src/main/java/com/example/blog/dao/repository/CommentSearchRepository.java
@@ -2,12 +2,14 @@ package com.example.blog.dao.repository;
 
 import com.example.blog.domain.elasticDao.SrchbleCommt;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by debnathchatterjee on 19/05/19.
  */
+@Repository
 public interface CommentSearchRepository extends ElasticsearchRepository<SrchbleCommt, String> {
 
     List<SrchbleCommt> findByWords(String word);
