29e11f89d328a0ebea6816a862b58c7b71b4a725
debnathchatterjee <debnathch@gmail.com>
2019-05-20 16:16:55 +0530

    FIRST COMMIT

--- src/main/java/com/example/blog/dao/SearchCmmtsWithWordService.java
+++ src/main/java/com/example/blog/dao/SearchCmmtsWithWordService.java
@@ -1,6 +1,6 @@
 package com.example.blog.dao;
 
-import com.example.blog.dao.repository.CommentSearchRepository;
+//import com.example.blog.dao.repository.CommentSearchRepository;
 import com.example.blog.domain.elasticDao.SrchbleCommt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
